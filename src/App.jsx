44eda57582e24d96ca77b11f556fc579c2e2a9d3
Gustavo Viana <167935951+guvdsz@users.noreply.github.com>
2024-12-16 09:22:49 -0300

    reposivity and some fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,47 @@
-import { useState } from 'react'
-import './App.css'
-import Header from './components/Header'
-import FormSection from './components/FormSection'
-import { getProject } from "./IA.js"
-import Project from './components/Project.jsx'
+import { useState, useRef, useEffect } from "react";
+import "./App.css";
+import Header from "./components/Header";
+import FormSection from "./components/FormSection";
+import { getProject } from "./IA.js";
+import Project from "./components/Project.jsx";
+
 function App() {
-const [techList, setTechList] = useState(["JavaScript"])
-const [project, setProject] = useState("")
-const [loading, setLoading] = useState(false)
-async function suggestProject() {
-  setLoading((prevLoading) => {
-    return !prevLoading
-  })
-  const response = await getProject(techList)
-  setProject(response)
-  setLoading((prevLoading) => {
-    return !prevLoading
-  })
-}
+  const [techList, setTechList] = useState(["JavaScript"]);
+  const [project, setProject] = useState("");
+  const [loading, setLoading] = useState(false);
+  const projectWrapper = useRef(null);
+
+  useEffect(() => {
+    if (project && projectWrapper.current) {
+      projectWrapper.current.scrollIntoView({ behavior: "smooth" });
+    }
+  }, [project]);
+
+  async function suggestProject() {
+    setLoading(true);
+    const response = await getProject(techList);
+    setProject(response);
+    setLoading(false);
+  }
+
   return (
     <>
       <main>
-        <Header/>
-        <FormSection setTechList={setTechList} techList={techList} suggestProject={suggestProject} loadingClass={loading}/>
-        {project && <Project project={project}/>}
+        <Header />
+        <FormSection
+          setTechList={setTechList}
+          techList={techList}
+          suggestProject={suggestProject}
+          loadingClass={loading}
+        />
+        {project && (
+          <div className="project-wrapper" ref={projectWrapper}>
+            <Project project={project} />
+          </div>
+        )}
       </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
