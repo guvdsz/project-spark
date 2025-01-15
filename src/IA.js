7987d429a28c9246edbcb362892ea798ffe8a66b
Gustavo Viana <167935951+guvdsz@users.noreply.github.com>
2025-01-15 16:28:03 -0300

    AI model update

--- src/IA.js
+++ src/IA.js
@@ -5,7 +5,7 @@ export async function getProject(list) {
   const stringTechList = list.join(", ");
 try {
     const response = await hf.chatCompletion({
-        model: "mistralai/Mixtral-8x7B-Instruct-v0.1",
+        model: "mistralai/Mistral-7B-Instruct-v0.3",
         messages: [
             { role: "system", content: `You are a project assistant that receives a list of technologies the user is proficient in and suggests 3 super creative projects that can be done using those technologies. You need to use all the provided technologies. The suggested projects should include only the provided technologies but can include any dependencies of those. Format the response in markdown to facilitate rendering on the page. Respond as if you are speaking directly to the user. Provide the project ideas without any introductory text. The project ideas should be VERY detailed, guiding the user clearly. Do not include any introductory phrases like "Sure, I can suggest".` },
             { role: "user", content: `I have knowledge about ${stringTechList}. Give me NEW project ideas now!` },
